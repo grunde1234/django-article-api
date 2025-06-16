4fefc97b8f43c9a01a6e076081fe25d1ea835c83
Okafor Ebuka <oebuka198@gmail.com>
2025-06-16 14:30:42 +0100

    cloud require

--- news_app/serializers.py
+++ news_app/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from django.shortcuts import get_object_or_404
 from .models import JournalistModel, CategoryModel, ArticleModel
 
 class JournalistSerializer(serializers.ModelSerializer):
@@ -22,13 +21,13 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ArticleSerializer(serializers.ModelSerializer):
-    image_select = serializers.ImageField(required=False)
+    image_select = serializers.SerializerMethodField()  # This will handle Cloudinary URL
+
     author_email = serializers.EmailField(write_only=True)
     author_email_display = serializers.EmailField(source='author.email', read_only=True)
 
     category_name = serializers.CharField(write_only=True)
-    category_name_display = serializers.CharField(source='category.name', read_only=True)
-    image_select = serializers.CharField()  
+    category_name_display = serializers.CharField(source='category.cat_name', read_only=True)
 
     class Meta:
         model = ArticleModel
@@ -37,9 +36,14 @@ class ArticleSerializer(serializers.ModelSerializer):
             'author_email',
             'image_select',
             'author_email_display',
-            'category_name',          # input
-            'category_name_display',  # output
+            'category_name',
+            'category_name_display',
         ]
+
+    def get_image_select(self, obj):
+        if obj.image_select:
+            return obj.image_select.url
+        return None
 
     def create(self, validated_data):
         author_email = validated_data.pop('author_email')
